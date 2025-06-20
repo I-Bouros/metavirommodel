Correct formula for seasonal birth rate

metavirommodel/_environment.py
@@ -160,8 +160,15 @@ class BirthRatePrec(Environment):
                 raise ValueError('All parameters must be => 0.')
 
     def _set_rate_rule(self):
-        """
+        r"""
         Computes the value of the birth rates based on a rainfall criterion.
+
+        .. math::
+            b(t) = |b_0 \sin(2 pi (\frac{\lfloor t/7 \rfloor \}{52} - \omega))
+            | + b_0 \sin(2 pi (\frac{\lfloor t/7 \rfloor \}{52} - \omega))
+
+        where :math:`b_M` is the baseline birth rate and :math:`\rho` is the
+        birth rate phase used to compute the birth rate.
 
         """
         self.birth_rate = lambda t: self.bM * self.avg_prec[
@@ -224,13 +231,20 @@ class BirthRateSeason(Environment):
                 raise ValueError('All parameters must be => 0.')
 
     def _set_rate_rule(self):
-        """
+        r"""
         Computes the value of the birth rates based on a seasonality criterion.
+
+        .. math::
+            b(t) = |b_0 \sin(2 pi (\frac{\lfloor t/7 \rfloor \}{52} - \omega))
+            | + b_0 \sin(2 pi (\frac{\lfloor t/7 \rfloor \}{52} - \omega))
+
+        where :math:`b_0` is the baseline birth rate and :math:`\omega` is the
+        birth rate phase used to compute the birth rate.
 
         """
 
         self.birth_rate = lambda t: np.abs(
-            self.b0 * np.sin(2*math.pi*(np.floor(t/7)/52 - self.w))) - \
+            self.b0 * np.sin(2*math.pi*(np.floor(t/7)/52 - self.w))) + \
             self.b0 * np.sin(2*math.pi*(np.floor(t/7)/52 - self.w))
 
     def __call__(self, t_cal):

metavirommodel/data/Uganda_precipitation.py
@@ -23,6 +23,7 @@ import pandas as pd
 from datetime import datetime
 import time
 from time import mktime
+import numpy as np
 
 
 def read_full_precipitation_data(pr_file: str):
@@ -93,7 +94,7 @@ def process_precipitation_data(
         daily_data = data[data['Day'] == t]
         newrow = pd.DataFrame([{
             'Day': t,
-            'Precmm': sum(daily_data['Precmm'].values)
+            'Precmm': np.sum(daily_data['Precmm'].values)
         }])
         precp = pd.concat([precp, newrow])
 
@@ -144,7 +145,7 @@ def main():
     precipitation = process_precipitation_data(
         data,
         start_date='01/01/2020 06:00',
-        end_date='31/12/2021 15:00')
+        end_date='31/12/2022 15:00')
 
     # Transform recorded matrix of serial intervals to csv file
     precipitation.to_csv(

metavirommodel/tests/test_environment.py
@@ -132,8 +132,8 @@ class TestBirthRateSeason(unittest.TestCase):
 
         self.assertAlmostEqual(
             birth_rate(4.2),
-            np.abs(0.5*np.sin(-2*math.pi*0.15)) - 0.5*np.sin(-2*math.pi*0.15))
+            np.abs(0.5*np.sin(-2*math.pi*0.15)) + 0.5*np.sin(-2*math.pi*0.15))
         self.assertAlmostEqual(
             birth_rate(104.2),
-            np.abs(0.5*np.sin(2*math.pi*(14/52-0.15))) - 0.5*np.sin(
+            np.abs(0.5*np.sin(2*math.pi*(14/52-0.15))) + 0.5*np.sin(
                 2*math.pi*(14/52-0.15)))